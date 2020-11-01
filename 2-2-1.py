c69f65811f66288438c0dcd713c7a1d36d744bae
ikedaema2018 <ikedaema0817@gmail.com>
2020-11-01 18:11:05 +0900

    3-1-1 self

---
+++ 2-2-1.py
@@ -0,0 +1,6 @@
+data = [1,2,5,3,9,7]
+
+new_data = sorted(data, reverse=True)
+print(new_data[0])
+print(new_data[1])
+print(new_data[2])
---
+++ 3-1-1-self.py
@@ -0,0 +1,33 @@
+data = [5,2,4,6,1,3]
+new_data = [data[0]]
+
+def print_result(x, y):
+  for n in x:
+    print(n, end=' ')
+  for n in y:
+    print(n, end=' ')
+  print('\n')
+
+for idx, n in enumerate(data[1:]):
+  new_idx = idx + 1
+  print_result(new_data, data[new_idx:])
+  change_flag = False
+  for nidx, v in enumerate(new_data):
+    if n < v:
+      source_data = n
+      for i in range(nidx, len(new_data)):
+        tmp = new_data[i]
+        new_data[i] = source_data
+        source_data = tmp
+      new_data.append(source_data)
+      change_flag = True
+      break
+  if not change_flag:
+    new_data.append(n)
+print_result(new_data, [])
+    
+        
+    
+    
+       
+    
