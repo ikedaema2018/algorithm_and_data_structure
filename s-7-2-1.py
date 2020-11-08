c552407df06bcdabf4f8dfe32e2e5502360b6401
ikedaema2018 <ikedaema0817@gmail.com>
2020-11-08 13:48:28 +0900

    fix

---
+++ s-7-2-1.py
@@ -0,0 +1,17 @@
+def partition(A, first, last):
+  x = A[last]
+  i = first - 1
+  for j in range(first, last - 1):
+    if A[j] <= x:
+      i += 1
+      print(j)
+      tmp = A[i]
+      A[i] = A[j]
+      A[j] = tmp
+  A[last] = A[i + 1]
+  A[i + 1] = x
+  return A[i + 1]
+
+data = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11]
+print(partition(data, 0, len(data) - 1))
+print(data)
